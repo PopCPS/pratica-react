33369d3f750a2c82060938f6f47f7108876aa1b7
PopCPS <arturcoelho.cps@gmail.com>
2023-09-29 08:01:58 -0300

    Map test

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Marker } from '@googlemaps/react-wrapper'
 
 import './index.css'
 
-const StartButton = () =>{
+const MyApp = () => {
     return (
-        <button className='botao botao-iniciar' >Find us!</button>
-    )
-}
-
-const Form = () =>{
-    return (
-        <form className='form form-user-info'>
-            <Input valueType='text' />
-            <Input valueType='number' />
-            <Input valueType='text' />
-            <Input valueType='number' />
-        </form>
-    )
-}
-
-const Input = (valueType, inputName) =>{
-    const errorMessage = ['campo obrigatorio'] 
-    return (
-        <div className='input-field'>
-            <h4 className='input-error-message'>{errorMessage}</h4>
-            <input name={inputName} type={valueType} className='input-text-box'></input>
+        <div className="App">
+            <Map apiKey="AIzaSyBD6i3mFT2s3H5SA8KUCjlxRpchgJ2KA7g" />
         </div>
     )
 }
 
+function MyMarker() {
+    return <Marker position={{ lat: 37.7749, lng: -122.4194 }} />
+  }
+
+const Map = () => {
+    return (
+        <Map
+          apiKey="AIzaSyBD6i3mFT2s3H5SA8KUCjlxRpchgJ2KA7g"
+          defaultZoom={8}
+          defaultCenter={{ lat: 37.7749, lng: -122.4194 }}
+          defaultOptions={{ disableDefaultUI: true }}
+        />
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Form />)
+root.render(<MyApp />)
+
+export default MyApp
